d9eace4b430f31ec1d60d0eee38f9e7c17593385
SEHEE <hsehee@udel.edu>
2023-03-03 17:40:04 -0500

    Completed tCSV function

--- src/nested.ts
+++ src/nested.ts
@@ -114,7 +114,21 @@ id,name,options,points,published
  * Check the unit tests for more examples!
  */
 export function toCSV(questions: Question[]): string {
-    return "";
+    const CSVQuestions = questions
+        .map(
+            (question: Question): string =>
+                question.id.toString() +
+                "," +
+                question.name +
+                "," +
+                question.options.length.toString() +
+                "," +
+                question.points.toString() +
+                "," +
+                question.published.toString()
+        )
+        .join("\n");
+    return "id,name,options,points,published\n" + CSVQuestions;
 }
 
 /**
