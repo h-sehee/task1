ad89c942a0866fe54ca5851ddac0d39e2acc8afc
SEHEE <hsehee@udel.edu>
2023-03-03 17:27:06 -0500

    Completed sumPublished Points function

--- src/nested.ts
+++ src/nested.ts
@@ -86,7 +86,14 @@ export function sumPoints(questions: Question[]): number {
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    const sum = questions
+        .filter((question: Question): boolean => question.published)
+        .reduce(
+            (currentTotal: number, question: Question) =>
+                currentTotal + question.points,
+            0
+        );
+    return sum;
 }
 
 /***
