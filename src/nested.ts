759c34a96bd0190b80a712fed9eca51f1343adf2
SEHEE <hsehee@udel.edu>
2023-03-03 17:42:27 -0500

    Completed publishAll function

--- src/nested.ts
+++ src/nested.ts
@@ -153,7 +153,14 @@ export function makeAnswers(questions: Question[]): Answer[] {
  * each question is now published, regardless of its previous published status.
  */
 export function publishAll(questions: Question[]): Question[] {
-    return [];
+    const newQuestions = questions.map(
+        (question: Question): Question => ({
+            ...question,
+            options: [...question.options],
+            published: true
+        })
+    );
+    return newQuestions;
 }
 
 /***
